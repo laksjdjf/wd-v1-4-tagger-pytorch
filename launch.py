55dafa5ae0cc25ae16206648818ec5b1b9b2180f
laksjdjf <aikasama@hotmail.co.jp>
2023-06-21 18:22:54 +0900

    ui実装

---
+++ launch.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from utils.preprocess import preprocess_image
+from vit.vit import ViT
+
+vit = ViT(3,448,9083)
+vit.load_state_dict(torch.load("wd-v1-4-vit-tagger-v2.ckpt"))
+csv = pd.read_csv("selected_tags.csv")
+
+def predict(image):
+    image = preprocess_image(image)
+    img = torch.tensor(image).permute(2,0,1).unsqueeze(0)
+    out = vit(img)[0]
+    return {csv["name"][i]:out[i].item() for i in range(9083)}
+
+import gradio as gr
+demo = gr.Interface(fn=predict, 
+             inputs=gr.inputs.Image(type="pil"),
+             outputs=gr.outputs.Label(num_top_classes=20),
+             )
+demo.launch(debug=True)
---
+++ utils/preprocess.py
@@ -0,0 +1,23 @@
+#ref:https://github.com/kohya-ss/sd-scripts/blob/main/finetune/tag_images_by_wd14_tagger.py
+
+import cv2
+from PIL import Image
+IMAGE_SIZE = 448
+
+def preprocess_image(image):
+    image = np.array(image)
+    image = image[:, :, ::-1]  # RGB->BGR
+
+    # pad to square
+    size = max(image.shape[0:2])
+    pad_x = size - image.shape[1]
+    pad_y = size - image.shape[0]
+    pad_l = pad_x // 2
+    pad_t = pad_y // 2
+    image = np.pad(image, ((pad_t, pad_y - pad_t), (pad_l, pad_x - pad_l), (0, 0)), mode="constant", constant_values=255)
+
+    interp = cv2.INTER_AREA if size > IMAGE_SIZE else cv2.INTER_LANCZOS4
+    image = cv2.resize(image, (IMAGE_SIZE, IMAGE_SIZE), interpolation=interp)
+
+    image = image.astype(np.float32)
+    return image
